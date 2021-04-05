62cf481dc0a04c7f61bfa4541803275c0f100e06
Kyle Lamse <kylelamse29@gmail.com>
2021-04-05 13:02:05 -0500

    Implement main

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/dgraph-io/badger/v3"
+	abciserver "github.com/tendermint/tendermint/abci/server"
+	"github.com/tendermint/tendermint/libs/log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+var socketAddr string
+
+func init() {
+	flag.StringVar(&socketAddr, "socket-addr", "unix://example.sock", "Unix domain socket address")
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open badger db: %v", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+	app := NewKVStoreApplication(db)
+	flag.Parse()
+
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	server := abciserver.NewSocketServer(socketAddr, app)
+	server.SetLogger(logger)
+
+	if err := server.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "error starting socket server: %v", err)
+		os.Exit(1)
+	}
+
+	defer server.Stop()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	os.Exit(0)
 }
